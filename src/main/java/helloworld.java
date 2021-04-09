156a93f29251f7ca25fa04e65ade64eec11437e7
Priya J <priya.j@thoughtworks.com>
2021-04-09 11:31:20 +0530

    fourth commit

--- src/main/java/helloworld.java
+++ src/main/java/helloworld.java
@@ -1,11 +1,8 @@
 public class helloworld {
     public static void main(String[] args) {
         System.out.println("hello world");
-<<<<<<< HEAD
         System.out.println("third line");
-=======
         System.out.println("next line");
->>>>>>> 5f9a7ede2f42cab567c269918c51c7cad3adf8e6
     }
 
 }
